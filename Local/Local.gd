c0c50e41f5d74ca62f653a3f2273505750876b8e
Arnimox <justreqofficial@gmail.com>
2024-09-26 10:46:24 -0400

    Implement body composite rework

--- Local/Local.gd
+++ Local/Local.gd
@@ -4,44 +4,60 @@ class_name Local extends CharacterBody2D
 @onready var ageManager: AgeManager = $AgeManager
 @onready var movementManager: MovementManager = $MovementManager
 
-@onready var spriteShirt := $Sprites/SpriteShirt
+@onready var spriteBody := $Sprites/SpriteBody
 @onready var spritePants := $Sprites/SpritePants
 @onready var spriteShoes := $Sprites/SpriteShoes
-@onready var spriteHair := $Sprites/SpriteHair
-@onready var spriteHead := $Sprites/SpriteHead
-@onready var spriteTorso := $Sprites/SpriteTorso
 @onready var spriteArms := $Sprites/SpriteArms
-@onready var spriteLegs := $Sprites/SpriteLegs
+@onready var spriteShirtBody := $Sprites/SpriteShirtBody
+@onready var spriteShirtArms := $Sprites/SpriteShirtArms
+@onready var spriteHair := $Sprites/SpriteHair
 
-@export_category("Properties")
-@export var world: World
-@export var sex: Utils.Sex
-@export var fullName: String
-@export var occupation: Utils.LocalOccupation
+var textures := {}
 
-@export_category("Colors")
-@export var skinColor: Color
-@export var hairColor: Color
-@export var shirtColor: Color
-@export var pantsColor: Color
-@export var shoesColor: Color
+var world: World
+var sex: Utils.Sex
+var fullName: String
+var occupation: Utils.LocalOccupation
 
-@export_category("Look")
-@export var shirt: int
-@export var pants: int
-@export var shoes: int
-@export var hair: int
+var skinColor: Color:
+	set(value):
+		skinColor = value
 
-@export_category("State")
-@export var state: Utils.State
+var hairColor: Color:
+	set(value):
+		hairColor = value
 
-func initialize(world: World, sex: Utils.Sex, fullName: String, occupation: Utils.LocalOccupation, skinColor: Color, hairColor: Color) -> void:
-	self.world = world
-	self.sex = sex
-	self.fullName = fullName
-	self.occupation = occupation
-	self.skinColor = skinColor
-	self.hairColor = hairColor
+var shirtColor: Color:
+	set(value):
+		shirtColor = value
+
+var pantsColor: Color:
+	set(value):
+		pantsColor = value
+
+var shoesColor: Color:
+	set(value):
+		shoesColor = value
+
+var shirt: int:
+	set(value):
+		shirt = value
+
+var pants: int:
+	set(value):
+		pants = value
+
+var shoes: int:
+	set(value):
+		shoes = value
+
+var hair: int:
+	set(value):
+		hair = value
+
+var state: Utils.State:
+	set(value):
+		state = value
 
 var faith := 1.0
 
@@ -78,6 +94,19 @@ var motiveEnergy := 1.0:
 func setMoodlet(value: float):
 	return 0
 
+func initialize(world: World, sex: Utils.Sex, fullName: String, occupation: Utils.LocalOccupation, skinColor: Color, hairColor: Color) -> void:
+	self.world = world
+	self.sex = sex
+	self.fullName = fullName
+	self.occupation = occupation
+	self.skinColor = skinColor
+	self.hairColor = hairColor
+
+func loadTextures():
+	for i in DirAccess.get_directories_at("res://Local/Textures"):
+		for j in Array(DirAccess.get_files_at("res://Local/Textures/" + i)).filter(func(e): return e.get_extension() == "png"):
+			textures[j.left(-4)] = load("res://Local/Textures/" + i + "/" + j)
+
 func die():
 	if Main.focusedNode == self:
 		Main.focusedNode = null
@@ -85,35 +114,17 @@ func die():
 	state = Utils.State.Dead
 	process_mode = PROCESS_MODE_DISABLED
 
+func _init() -> void:
+	loadTextures()
+
 func _ready() -> void:
 	ageManager.lifespanSeconds = Utils.timeUnitsToSeconds(Utils.TimeUnit.Year) * 40
-	shoes = 1
-	pants = 1
-	shirt = 1
-	hair = sex + 1
+	hair = sex
 	shoesColor = Color("453018")
 	pantsColor = Color("292e55") if sex == Utils.Sex.Male else Color("4b4faa")
 	shirtColor = Color("7d0f0f") if sex == Utils.Sex.Male else Color("588842")
-	
-	spriteHead.self_modulate = skinColor
-	spriteTorso.self_modulate = skinColor
-	spriteArms.self_modulate = skinColor
-	spriteLegs.self_modulate = skinColor
-	spriteShoes.self_modulate = shoesColor
-	spritePants.self_modulate = pantsColor
-	spriteShirt.self_modulate = shirtColor
-	spriteHair.self_modulate = hairColor
 
 func _physics_process(delta: float) -> void:
-	spriteHead.texture = load("res://Local/{}/Head{}.png".format([Utils.State.keys()[state], Utils.State.keys()[state]], "{}"))
-	spriteTorso.texture = load("res://Local/{}/Torso{}.png".format([Utils.State.keys()[state], Utils.State.keys()[state]], "{}"))
-	spriteArms.texture = load("res://Local/{}/Arms{}.png".format([Utils.State.keys()[state], Utils.State.keys()[state]], "{}"))
-	spriteLegs.texture = load("res://Local/{}/Legs{}.png".format([Utils.State.keys()[state], Utils.State.keys()[state]], "{}"))
-	spriteShoes.texture = load("res://Local/{}/Shoes{}{}.png".format([Utils.State.keys()[state], shoes, Utils.State.keys()[state]], "{}"))
-	spritePants.texture = load("res://Local/{}/Pants{}{}.png".format([Utils.State.keys()[state], pants, Utils.State.keys()[state]], "{}"))
-	spriteShirt.texture = load("res://Local/{}/Shirt{}{}.png".format([Utils.State.keys()[state], shirt, Utils.State.keys()[state]], "{}"))
-	spriteHair.texture = load("res://Local/{}/Hair{}{}.png".format([Utils.State.keys()[state], sex + 1, Utils.State.keys()[state]], "{}"))
-	
 	animationPlayer.play(Utils.State.keys()[state] + Utils.Direction.keys()[Utils.vectorToDirection(movementManager.lastMovedDirection)])
 	
 	motiveHunger -= (world.timeManager.ingameTimestamp - world.timeManager.oldIngameTimestamp) / (Utils.timeUnitsToSeconds(Utils.TimeUnit.Day, 55) * (2 if state == Utils.State.Sleeping else 1))
